62a26c26ce1c3ed8795028d5449320c952e9d883
Stefan Saraev <stefan@saraev.ca>
2019-02-07 22:29:09 +0200

    [update] mmdvmcal-f9bb1b0fea

--- packages/extra/mmdvmcal/package.mk
+++ packages/extra/mmdvmcal/package.mk
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: GPL-2.0-only
 
 PKG_NAME="mmdvmcal"
-PKG_VERSION="8af8dae187"
+PKG_VERSION="f9bb1b0fea"
 PKG_SITE="https://github.com/g4klx/MMDVMCal"
 PKG_URL="$PKG_SITE/archive/$PKG_VERSION.tar.gz"
 
